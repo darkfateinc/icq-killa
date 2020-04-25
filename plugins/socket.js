2d8e67044d05d6ae5d8143c7e5db9128bd1d5cde
darkfateinc <darkfateinc@yandex.ru>
2020-04-25 13:20:17 +0300

    server socket fixed 1

--- plugins/socket.js
+++ plugins/socket.js
@@ -4,7 +4,7 @@ import VueSocketIO from 'vue-socket.io'
 export default function ({store}){
   Vue.use(new VueSocketIO({
     debug: false,
-    connection: "http://https://floating-castle-80355.herokuapp.com/",
+    connection: "http://https://floating-castle-80355.herokuapp.com:3000",
     vuex: {
       store,
       actionPrefix: 'SOCKET_',
